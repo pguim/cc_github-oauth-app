ef279ffb9ce14e74b43407f9779043b84782df1c
guimmie <pguimera@gmail.com>
2025-05-02 11:47:56 +0200

    Make part 1 to 3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /* 
  * Package Imports
 */
-
+const session = require('express-session')
+const passport = require('passport')
+const GitHubStrategy = require('passport-github2').Strategy
 const path = require("path");
 require("dotenv").config();
 const express = require('express');
 const partials = require('express-partials');
 
-
 const app = express();
-
 
 /*
  * Variable Declarations
@@ -22,25 +22,29 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
-
-
-
-
-
-
+passport.use(new GitHubStrategy({
+  "clientID": GITHUB_CLIENT_ID,
+  "clientSecret": GITHUB_CLIENT_SECRET,
+  "callbackURL": "http://localhost:3000/auth/github/callback"
+}, (accessToken, refreshToken, profile, done) => { done(null, profile) }))
 
 /*
  *  Express Project Setup
 */
-
+app.use(session(
+  {
+    secret: 'codecademy',
+    resave: false,
+    saveUninitialized: false
+  }
+))
+app.use(passport.initialize())
+app.use(passport.session())
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-
-
-
 
 /*
  * Routes
@@ -62,9 +66,6 @@ app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
-
-
-
 
 /*
  * Listener
